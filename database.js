b7567e5138f5edf64e0224ce189e20047db89b26
MrFibonacc1 <huzaifarehan004@gmail.com>
2024-03-28 13:06:22 -0400

    first commit

--- database.js
+++ database.js
@@ -1,7 +1,12 @@
 const {Client} = require('pg')
+require('dotenv').config();
 
 const client = new Client({
- 
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    port: process.env.DB_PORT,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 })
 
 client.connect();
