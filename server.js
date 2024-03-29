d52e493f3869094f3e5ac2348bd86a32bcded4de
MrFibonacc1 <huzaifarehan004@gmail.com>
2024-03-29 18:54:43 -0400

    Login System Complete

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 // Import required modules
 const express = require('express');
 const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
 const { executeQuery, client } = require('./database');
 
-
-
+const initializePassport = require("./config");
+initializePassport(passport);
 // Create an Express application
 const app = express();
 const port = 4000; // Port number to listen on
@@ -13,32 +16,50 @@ app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.use(express.static('public'));
 
+app.use(
+  session({
+    // Key we want to keep secret which will encrypt all of our information
+    secret: process.env.SESSION_SECRET,
+    // Should we resave our session variables if nothing has changes which we dont
+    resave: false,
+    // Save empty value if there is no vaue which we do not want to do
+    saveUninitialized: false
+  })
+);
+
+// Funtion inside passport which initializes passport
+app.use(passport.initialize());
+// Store our variables to be persisted across the whole session. Works with app.use(Session) above
+app.use(passport.session());
+app.use(flash());
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('/students', async (req, res) => {
-  const result = await client.query('SELECT * FROM students');
-  console.log(result.rows);
-  // try {
-  //   const students = await executeQuery('SELECT * FROM students');
-  //   res.json(students);
-  //   console.log("test")
-  //   console.log(students)
-  // } catch (error) {
-  //   console.error('Error fetching students:', error);
-  //   res.status(500).json({ error: 'Internal Server Error' });
-  // }
-});
 
-app.get("/users/login", (req, res) => {
+app.get("/users/login", checkAuthenticated, (req, res) => {
   res.render("login.ejs");
 });
 
-app.get("/users/register", (req, res) => {
+app.get("/users/register",checkAuthenticated, (req, res) => {
   res.render("register.ejs");
 });
+
+app.get("/users/dashboard", checkNotAuthenticated, (req, res) => {
+  console.log(req.isAuthenticated());
+  res.render("dashboard", { user: req.user.full_name });
+});
+
+app.get("/users/logout", (req, res) => {
+  req.logout(function(err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('/');
+  });
+});
+
 
 app.post("/users/register", async (req, res) => {
   let { name, email, password, password2 } = req.body;
@@ -95,7 +116,7 @@ app.post("/users/register", async (req, res) => {
                 throw err;
               }
               console.log(results.rows);
-              // req.flash("success_msg", "You are now registered. Please log in");
+              req.flash("success_msg", "You are now registered. Please log in");
               res.redirect("/users/login");
               console.log("test");
             }
@@ -105,6 +126,29 @@ app.post("/users/register", async (req, res) => {
     );
   }
 });
+
+app.post(
+  "/users/login",
+  passport.authenticate("local", {
+    successRedirect: "/users/dashboard",
+    failureRedirect: "/users/login",
+    failureFlash: true
+  })
+);
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/users/dashboard");
+  }
+  next();
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/users/login");
+}
 
 // Start the server and listen on the specified port
 app.listen(port, () => {
