fe9740fe3dd7453e62d7390010b489241bbded08
Huzaifa Rehan <huzaifa@Huzaifas-MacBook-Pro.local>
2024-06-22 22:59:34 -0400

    vercel

--- database.js
+++ database.js
@@ -2,11 +2,7 @@ const {Client} = require('pg')
 require('dotenv').config();
 
 const client = new Client({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    port: process.env.DB_PORT,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    connectionString: process.env.POSTGRES_URL,
 })
 
 client.connect();
